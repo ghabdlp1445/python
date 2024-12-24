a69a7c6d1b1c20ada19af7e22d671a2a4fb1362f
ghabdlp1445 <abdlp1445@gmail.com>
2024-12-24 09:09:30 +0500

    Add files via upload

---
+++ mimo.py
@@ -0,0 +1,55 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Oct  5 18:34:54 2024
+
+@author: NEXT NOUT
+"""
+
+# print("hello world")
+
+# >>>>>>>>>>>>>> ! BOT
+
+# print("What is your name?: ")
+# name = input()
+# name = input("Hello what is your name?: ")
+# print(f"Nice to meet you, {name}!")
+# age_input = input("How old are you?: ")
+# age = int(age_input)
+# bot_age = 1
+# age_difference = age - bot_age
+# print(f"You are {age_difference} year older than me. I'm only {bot_age} years old!")
+# color = input("What is your favoruite color?: ")
+# print(f"Oh {color} is a very beautiful color!")
+
+
+
+# >>>>>> ! New Password
+
+# (password = 1)
+# old_p = "hello123"
+# new_p = "goodbye321"
+# compare_o_n = old_p != new_p
+# repeat_n_p = "goodbye321"
+# compare_n = new_p == repeat_n_p
+
+# print(f"Is new password different from old password? {compare_o_n}")
+# print(f"Has new password been introduced correctly? {compare_n}")
+
+
+# >>>>>> ! Getting ticket
+
+# age = 21
+# adult_ticket = age >= 18
+
+# print(f"Can he buy an adult ticket?: {adult_ticket}")
+
+
+
+# >>>>>>> !
+
+
+
+
+
+
+
